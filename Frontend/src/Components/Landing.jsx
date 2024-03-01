4194efe32de05cf4a08a2126dffc338716e4739a
dhruvil-sheth30 <dhruvildeepaksheth@gmail.com>
2024-03-01 16:33:16 +0530

    Added entities and routes

---
+++ Frontend/src/Components/Landing.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+import './Landing.css';
+
+function Landing() {
+  return (
+    <div className="landing-page">
+      <div className='trans'></div>
+      <div className='container'>
+        <h1>Mediocre Big Budget Movies</h1>
+        <p>Welcome to our site! Explore a collection of pathetic big-budget movies that offer little to no entertainment value and may leave you questioning your life choices.</p>
+        <Link to="/home" className="explore-button">Explore Now</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
