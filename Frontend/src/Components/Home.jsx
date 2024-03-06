56affba91d98f9e536216c208a0add9d7adc344d
Dhruvil Sheth <dhruvilsheth@proton.me>
2024-03-06 17:59:39 +0530

    Add movie creation route and form component

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
 
   const fetchMovies = async () => {
     try {
-      const response = await fetch("https://five5-mediocremovieslist.onrender.com/read");
+      const response = await fetch("https://mediocre-movies.onrender.com/read");
       const data = await response.json();
       setMovies(data);
     } catch (error) {
@@ -42,8 +42,8 @@ function Home() {
               </form>
             </div>
             <div className="navbar-links">
-              <Link to="/">Home</Link>
-              <Link to="/about">About</Link>
+              <Link to="/Home">Home</Link>
+              <Link to="/Form">Add-Entity</Link>
               <Link to="/contact">Contact</Link>
             </div>
           </div>
